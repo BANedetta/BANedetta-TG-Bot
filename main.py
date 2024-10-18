5af7b3bf5b494b4f604fa4d79c6fb9febe2499f5
Taskov1ch <tynaev.acc@gmail.com>
2024-10-19 00:21:05 +0600

    1.29 | Add logging

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher
 from asyncio import get_event_loop, gather
 from banedetta_db import DB
+from bot.managers.log_manager import logger
 from config import config
 from dotenv import load_dotenv
 from os import getenv
@@ -17,6 +18,12 @@ dp = Dispatcher()
 async def start_bot():
 	from bot.routers.tg_router import rt
 	dp.include_routers(rt)
+
+	async def on_startup():
+		logger.info("Telegram bot is working")
+
+	dp.startup.register(on_startup)
+	await bot.delete_webhook(drop_pending_updates = True)
 	await dp.start_polling(bot)
 
 async def start_synchronization():
@@ -28,5 +35,8 @@ async def main():
 	await gather(start_bot(), start_synchronization())
 
 if __name__ == "__main__":
-	loop = get_event_loop()
-	loop.run_until_complete(main())
+	try:
+		loop = get_event_loop()
+		loop.run_until_complete(main())
+	except KeyboardInterrupt:
+		logger.info("Shutdown...")
